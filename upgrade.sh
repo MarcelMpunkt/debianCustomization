cf144c0ce27fe5b7a6c7a2c195654cc14de4eabd
MarcelMpunkt <m.dev@mailbox.org>
2020-04-08 00:35:40 +0200

    change file to update && upgrade script
    after test was successful, now change contents to upgrade script

---
+++ upgrade.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Execute this file directly from the commandline
+# wget --quiet --output-document=- https://raw.githubusercontent.com/MarcelMpunkt/handyDebianCustomization/master/upgrade.sh | sudo bash
+
+echo "Test"
