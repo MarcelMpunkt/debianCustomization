1ec15d1df49ecadab00cd582b309e670c36d95d7
MarcelMpunkt <m.dev@mailbox.org>
2020-04-08 00:48:00 +0200

    update/upgrade code

--- upgrade.sh
+++ upgrade.sh
@@ -3,4 +3,19 @@
 # Execute this file directly from the commandline
 # wget --quiet --output-document=- https://raw.githubusercontent.com/MarcelMpunkt/handyDebianCustomization/master/upgrade.sh | sudo bash
 
-echo "Test"
+ echo -n '-> Sync time:' &&\
+  timedatectl set-ntp 0 &&\
+  timedatectl set-ntp 1 &&\
+  echo ' ✓ done' &&\
+  sleep 1 &&\
+  echo '-> Updating: &&;\
+  sudo apt-get update &&\
+  echo '-> Full Upgrade:' &&\
+  sudo apt full-upgrade -y &&\
+  echo '-> Dist Upgrade' &&\
+  sudo apt dist-upgrade -y &&\
+  echo '-> Cleanig Cache:' &&\
+  sudo apt autoremove -y &&\
+  sudo apt clean -y &&\
+  echo ' ' &&\
+  echo 'System should be up-to-date  ✓'
