c2733cb9043825e0829181205bd9c6f51227327d
MarcelMpunkt <m.dev@mailbox.org>
2020-04-08 00:55:19 +0200

    changeing single to double quotes

--- upgrade.sh
+++ upgrade.sh
@@ -3,19 +3,19 @@
 # Execute this file directly from the commandline
 # wget --quiet --output-document=- https://raw.githubusercontent.com/MarcelMpunkt/handyDebianCustomization/master/upgrade.sh | sudo bash
 
- echo -n '-> Sync time:' &&\
+ echo -n "-> Sync time:" &&\
   timedatectl set-ntp 0 &&\
   timedatectl set-ntp 1 &&\
-  echo ' ✓ done' &&\
+  echo " ✓ done" &&\
   sleep 1 &&\
-  echo '-> Updating:' &&\
+  echo "-> Updating:" &&\
   sudo apt-get update &&\
-  echo '-> Full Upgrade:' &&\
+  echo "-> Full Upgrade:" &&\
   sudo apt full-upgrade -y &&\
-  echo '-> Dist Upgrade' &&\
+  echo "-> Dist Upgrade" &&\
   sudo apt dist-upgrade -y &&\
-  echo '-> Cleanig Cache:' &&\
+  echo "-> Cleanig Cache:" &&\
   sudo apt autoremove -y &&\
   sudo apt clean -y &&\
-  echo ' ' &&\
-  echo 'System should be up-to-date  ✓'
+  echo -n &&\
+  echo "System should be up-to-date  ✓";
